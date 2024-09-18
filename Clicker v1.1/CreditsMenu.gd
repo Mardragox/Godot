9b6bda990b476815fccb1da2db79b7c65468b872
Mardrago <112937342+Mardragox@users.noreply.github.com>
2024-09-18 18:16:46 +0200

    Delete Clicker v1.1 directory

--- Clicker v1.1/CreditsMenu.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-
-
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://../SettingsMenu/settings_menu.tscn")
--- Clicker v1.1/MainMenu.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://../main/main.tscn")
